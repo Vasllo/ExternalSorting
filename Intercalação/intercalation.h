bc25c1acb1be1693e13c1e27e5c82c8867ef4cdd
Mateus Campello <31205200+Vasllo@users.noreply.github.com>
2022-09-07 23:40:35 -0300

    Add files via upload

--- Intercalação/intercalation.h
+++ Intercalação/intercalation.h
@@ -1,3 +1,4 @@
+// Legenda: Nível = profundidade
 #include "cliente.h"
 #include <math.h>
 
@@ -21,15 +22,20 @@ void liberarNoh (arvoreVencedor* noh) {
     noh = NULL;
 }
 
-//FILE** carregarClientes ()
+// < - !! - > Protótipo não-terminado
+//FILE** carregarClientes () {}
 
+// Adiciona todos arquivos à arvore
 arvoreVencedor* adicionarFolhas (arvoreVencedor* raiz, FILE** clientes, int nClientes) {
+    // Array dinâmico para manter referência de todas folhas
+    // < - !! - > Passar responsabilidade de alocação e desalocação para função auxiliar?
     arvoreVencedor** folhas = (arvoreVencedor**) malloc(sizeof(arvoreVencedor*)*nClientes);
     for(int i=0; i<nClientes; i++) {
         folhas[i] = (arvoreVencedor*) malloc(sizeof(arvoreVencedor));
     }
     Cliente* aux = NULL;
 
+    // Construção das folhas com o ponteiro de cada arquivo e o código de cliente do primeiro cliente do arquivo
     for(int i=0; i<nClientes; i++) {
         aux = lerClienteDeArquivo(clientes[i], 0);
         folhas[i]->vencedor = aux->codCliente;
@@ -44,12 +50,13 @@ arvoreVencedor* adicionarFolhas (arvoreVencedor* raiz, FILE** clientes, int nCli
     }
     free(folhas);
     folhas = NULL;
-    // Passar responsabilidade de alocação e desalocação para função auxiliar?
     
     return raiz;
 }
 
+// Função interna recursiva para construir os nós internos e retornar a árvore completa
 arvoreVencedor* _adicionarNos (arvoreVencedor* raiz, arvoreVencedor** nos, int nFolhas) {
+    // Condição de parada, caso haja 2 ou menos sub-arvores
     if(nFolhas <= 2) {
         switch (nFolhas) {
             case 2:
@@ -69,17 +76,34 @@ arvoreVencedor* _adicionarNos (arvoreVencedor* raiz, arvoreVencedor** nos, int n
             default:
                 return raiz;
     }
-    arvoreVencedor** nos = (arvoreVencedor**) malloc(sizeof(arvoreVencedor*)*ceil(nFolhas/2));
-    for(int i=0; i<nFolhas; i++) {
-        nos[i] = (arvoreVencedor*) malloc(sizeof(arvoreVencedor));
+    // Caso contrário, são alocados ceil(nFolhas/2) nós para construir o nível superior da árvore
+    // < - !! - > Passar responsabilidade de alocação e desalocação para função auxiliar?
+    int nNos = ceil(nFolhas/2);
+    arvoreVencedor** novosNos = (arvoreVencedor**) malloc(sizeof(arvoreVencedor*)*nNos);
+    for(int i=0; i<nNos; i++) {
+        novosNos[i] = (arvoreVencedor*) malloc(sizeof(arvoreVencedor));
     }
-    // Passar responsabilidade de alocação e desalocação para função auxiliar?
+    // Caso haja um número ímpar de sub-árvores, o último nó copia o último nó do nível inferior, subindo-o de nível em vez de torná-lo filho único (proibido)
+    if(nFolhas%2 == 1) {
+        novosNos[nNos-1] = nos[nFolhas-1];
+        nNos--;
+    }
+    // Cada nó do nível atual tem 2 sub-árvores do nível inferior alocadas nos ponteiros esq/dir e o menor código das duas é colocado como vencedor
+    for(int i=0; i<nNos; i++) {
+        int flag=0, j=0;
+        novosNos[i]->esquerda = nos[j];
+        novosNos[i]->direita = nos[j+1];
 
-    
+        nos[j]->vencedor <= nos[j+1]->vencedor? flag=j : flag=j+1;
+        novosNos[i]->vencedor = nos[flag]->vencedor;
+        novosNos[i]->pVencedor = nos[flag]->pVencedor;
+        j+=2;
+    }
 
-    return raiz;
+    return raiz = _adicionarNos (raiz, nos, nFolhas); // ?
 }
 
+// < - !! - > Protótipo não-terminado
 void atualizarFolha () {
 
 }
